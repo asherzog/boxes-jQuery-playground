07d70935569a23c3d3d6c7fd28f2ceb6f15efb44
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-02 19:24:05 -0600

    starting events

--- boxes.js
+++ boxes.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+$(document).ready(function() {
+  var secret = $('#row3').find('#secretBox');
+  secret.css('background-color','white');
+  secret.append('<h1>Secret Box!</h1>');
+
+  // $('#row1 div').attr('class','boxtype3');
+  //
+  // $('#row4').children().last().css('display','none');
+  //
+  // $('.boxType1').css('background-color','white');
+  //
+  // var twos = $('#row2').children().slice(0,2);
+  // twos.removeClass('box boxType2 boxType3');
+  //
+  // $('div').not('.row').width(2);
+
+  // $('#container').click(function() {
+  //   console.log(event.pageX + ',' + event.pageY);
+  // })
+
+  $('.boxType1').append('<a href"http://www.galvanize.com/">Galvanize</a>');
+
+});
